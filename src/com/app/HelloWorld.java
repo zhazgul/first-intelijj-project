b9467275c8ce755b3ea1804fd8b42b277cbb1943
Zhaz <Zhazgulp@gmail.com>
2019-08-11 17:10:15 -0400

    First line added

---
+++ src/com/app/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.app;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/zhazgulpirmatova/Desktop/webdriver/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // step1 navigate to the google homepage
+        driver.get("http://.etsy.com");
+
+        System.out.println("Hello");
+
+    }
+
+}
